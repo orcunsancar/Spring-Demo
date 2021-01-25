61ae83decce5cbb875d5ef427b96db29c6d79e13
Orcun Sancar <eorcunsancar@gmail.com>
2021-01-26 00:19:11 +0300

    JpaRepository added

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,21 +1,22 @@
 package com.example.demo.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
-	public List<Student> getStudents() {
-		return List.of(
-				new Student(
-						1L,
-						"Maraim",
-						"mariam.jamal@gmail.com",
-						LocalDate.of(2000, Month.JANUARY, 5),
-						21)
-				);
+
+	private final StudentRepository studentRepository;
+
+	@Autowired
+	public StudentService(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
 	}
+
+	public List<Student> getStudents() {
+		return studentRepository.findAll();
+	}
+
 }
