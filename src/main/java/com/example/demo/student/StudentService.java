05271829a9369f1a00e6445d5168c7ba0f296aac
Orcun Sancar <eorcunsancar@gmail.com>
2021-01-26 14:40:45 +0300

    Adding student and checking student according to email is added

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,6 +1,7 @@
 package com.example.demo.student;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,15 @@ public class StudentService {
 
 	public List<Student> getStudents() {
 		return studentRepository.findAll();
+	}
+
+	public void addNewStudent(Student student) {
+		Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
+		
+		if(studentByEmail.isPresent()) {
+			throw new IllegalStateException("email taken");
+		}
+		studentRepository.save(student);		
 	}
 
 }
